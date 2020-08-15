d5e276492dedc52d78a00f638e5622a235aba9cc
Nobuhiko kiyasu <kiyasu.nobuhiko@gmail.com>
2020-08-15 18:56:05 +0900

    OK

---
+++ src/Assets/BlitTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlitTest : MonoBehaviour
+{
+    public RenderTexture testTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination){
+        Debug.Log(source.dimension.ToString());
+        Graphics.Blit(source, null as RenderTexture);
+        Graphics.Blit(source, testTexture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
